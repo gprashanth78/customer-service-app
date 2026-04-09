4b4541a455b70559b998a6922d5fb971cd166827
Prashanth118 <golla.prashanth118@gmail.com>
2026-04-09 17:16:49 +0530

    Updated the datasource properties

--- src/main/java/com/prashanthit/entity/Customer.java
+++ src/main/java/com/prashanthit/entity/Customer.java
@@ -1,9 +1,6 @@
 package com.prashanthit.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -12,7 +9,7 @@ import lombok.Data;
 public class Customer {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer customerId;
     private String customerName;
     private String customerEmail;
